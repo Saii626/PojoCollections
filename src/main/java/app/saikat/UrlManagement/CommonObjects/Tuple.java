baf146bd66a347b91316f6cdbfade5c2e1856d1f
Saikat Dey <saii@localhost.localdomain>
2019-07-22 23:40:52 +0530

    Added common Tuple class

---
+++ src/main/java/app/saikat/UrlManagement/CommonObjects/Tuple.java
@@ -0,0 +1,12 @@
+package app.saikat.UrlManagement.CommonObjects;
+
+public class Tuple<X, Y> {
+    public X first;
+
+    public Y second;
+
+    public Tuple(X first, Y second) {
+        this.first = first;
+        this.second = second;
+    }
+}
--- src/main/java/app/saikat/UrlManagement/CommonObjects/WebsocketMessageHandlers.java
+++ src/main/java/app/saikat/UrlManagement/CommonObjects/WebsocketMessageHandlers.java
@@ -6,17 +6,6 @@ import java.util.Map;
 
 public abstract class WebsocketMessageHandlers {
 
-    public static class Tuple<X, Y> {
-        public X first;
-
-        public Y second;
-
-        public Tuple(X first, Y second) {
-            this.first = first;
-            this.second = second;
-        }
-    }
-
     protected Map<Class<?>, List<Tuple<Class<?>, String>>> handlers = new HashMap<>();
 
     public Map<Class<?>, List<Tuple<Class<?>, String>>> getHandlers() {

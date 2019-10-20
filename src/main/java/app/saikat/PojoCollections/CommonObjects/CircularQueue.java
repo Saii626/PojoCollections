5760a48a84286665ba90d4cb896b01484f73fa21
Saikat Dey <saikat626@gmail.com>
2019-10-20 23:51:05 +0530

    Added few data structures

---
+++ src/main/java/app/saikat/PojoCollections/CommonObjects/CircularQueue.java
@@ -0,0 +1,41 @@
+package app.saikat.PojoCollections.CommonObjects;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.function.BinaryOperator;
+import java.util.function.Consumer;
+
+public class CircularQueue<T> {
+
+    private final List<T> list;
+    private final int MAX_SIZE;
+    private int index;
+    private int size;
+
+    public CircularQueue(int size) {
+        this.list = Collections.synchronizedList(new ArrayList<>(size));
+        this.index = 0;
+        this.size = 0;
+        this.MAX_SIZE = size;
+    }
+
+    public void put(T t) {
+        synchronized (list) {
+            list.add(index, t);
+            ++index;
+
+            size = size == MAX_SIZE ? MAX_SIZE : ++size;
+            if (index == MAX_SIZE) index = 0;
+        }
+    }
+
+    public void forEach(Consumer<? super T> consumer) {
+        list.forEach(consumer);
+    }
+
+    public T reduce(BinaryOperator<T> operation) {
+        return list.stream().reduce(operation).orElseThrow(() -> new NoSuchElementException());
+    }
+}
--- src/main/java/app/saikat/PojoCollections/CommonObjects/Tuple.java
+++ src/main/java/app/saikat/PojoCollections/CommonObjects/Tuple.java
@@ -9,4 +9,19 @@ public class Tuple<X, Y> {
         this.first = first;
         this.second = second;
     }
+
+    public static <X, Y> Tuple<X, Y> of(X first, Y second) {
+        return new Tuple<X, Y>(first, second);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Tuple) {
+            Tuple<?, ?> t = (Tuple<?, ?>) obj;
+            return (first != null ? first.equals(t.first) : t.first == null)
+                    && (second != null ? second.equals(t.second) : t.second == null);
+        } else {
+            return false;
+        }
+    }
 }
